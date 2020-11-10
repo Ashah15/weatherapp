c7fc9abe9a85a2f5d0756df538a861e4cb884720
Ashah15 <ashaabdillahi53@gmail.com>
2020-11-10 18:48:24 +0300

    resolved the error

--- src/index.js
+++ src/index.js
@@ -4,13 +4,13 @@ import {displayResults} from './displayResults'
 const searchBox = document.querySelector('.search-box');
 searchBox.addEventListener('keypress', setQuery);
 
-const setQuery = (event) => {
+function setQuery (event){
   if (event.keyCode === 13) {
     getResults(searchBox.value);
   }
 }
 
-const getResults = (query) =>{
+function getResults (query) {
   fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
     .then(weather => weather.json()).then(displayResults);
 }
